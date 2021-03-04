2440ee5b90f991ff89cb093887bab50d9eb28085
huhn511 <sehe89@gmail.com>
2021-03-04 01:14:33 +0100

    add build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# This is a comment!
+rm -f ./dist
+mkdir ./dist
+echo "Build de..."
+mdbook build ./src/de
+mkdir ./dist/de
+cp -r ./src/de/book/. ./dist/de
+echo "... de build finshed."
+echo "Build en..."
+mdbook build ./src/en
+mkdir ./dist/en
+cp -r ./src/en/book/. ./dist/en
+echo "... en build finshed."
+echo "Build index..."
+mdbook build
+cp -r ./book/. ./dist
+echo "... index build finshed."
