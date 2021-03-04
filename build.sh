864675a9eb0496247d2d8fa78aa0657096e3f63f
huhn511 <sehe89@gmail.com>
2021-03-04 01:27:02 +0100

    create folder

--- build.sh
+++ build.sh
@@ -4,12 +4,12 @@ rm -f ./dist
 mkdir ./dist
 echo "Build de..."
 mdbook build ./src/de
-mkdir ./dist/de
+mkdir ./dist/magazines/de
 cp -r ./src/de/book/. ./dist/magazines/de
 echo "... de build finshed."
 echo "Build en..."
 mdbook build ./src/en
-mkdir ./dist/en
+mkdir ./dist/magazines/en
 cp -r ./src/en/book/. ./dist/magazines/en
 echo "... en build finshed."
 echo "Build index..."
